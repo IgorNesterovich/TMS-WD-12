d8b47729db68a834201d10f8f2942a44e8fe6377
Igor Nesterovich <igornesterovich2000@gmail.com>
2024-04-08 17:01:31 +0300

    completed: completed task 26

--- HW_26/routes/api.php
+++ HW_26/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
+use App\Http\Controllers\CustomerController;
+use App\Http\Resources\ProductResource;
+use App\Models\Product;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +20,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('products', function() {
+    return ProductResource::collection(Product::paginate(10));
+});
+
+Route::patch('customers/update', [CustomerController::class, 'update']);
